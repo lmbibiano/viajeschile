7b21098df71406e5e9d97d0c07dcf0b11aeeb21e
Luis <lmbibianochile@gmail.com>
2024-03-06 21:23:55 -0300

    Agegamos poppover y tooltips

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,13 +1,16 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
 
 
 $(function() {
     var titulo = $("h2");
     titulo.on("click",function() {
-        titulo.css ("color", "violet");
+        titulo.css ("color", "red");
     });
 });
