1b6c19b7bda4f6af63a31ccbbeb5b3f6dd37b1e9
Luis <lmbibianochile@gmail.com>
2024-03-07 18:02:35 -0300

    correciones tooltips

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,16 +1,25 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-});
+
+$(function() {
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
+
+  var correo = $("#icon");
+  correo.click(function () {
+      alert ("Ven a disfrutar de Chile.....");
+      });
+  
+
+      var color = $("#h2");
+
+      color.dblclick(function () {
+          $(this).css("color", "red");
+          
+      });
+  
+
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
-
-
-
-$(function() {
-    var titulo = $("h2");
-    titulo.on("click",function() {
-        titulo.css ("color", "red");
-    });
-});
